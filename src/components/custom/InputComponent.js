13d17b3b1cc0b2ad0751a832d00c37cbb90f5f44
Kold <talininkold@gmail.com>
2021-05-04 13:39:34 +0700

    input component changed for password (show password button)

--- src/components/custom/InputComponent.js
+++ src/components/custom/InputComponent.js
@@ -1,6 +1,11 @@
+import { useState, useCallback } from 'react'
 import TextField from '@material-ui/core/TextField'
 import { Controller } from 'react-hook-form'
 import PropTypes from 'prop-types'
+import IconButton from '@material-ui/core/IconButton'
+import InputAdornment from '@material-ui/core/InputAdornment'
+import Visibility from '@material-ui/icons/Visibility'
+import VisibilityOff from '@material-ui/icons/VisibilityOff'
 
 const initialStyles = {
   variant: 'outlined',
@@ -17,6 +22,20 @@ function InputComponent ({
   rules,
   styles
 }) {
+  const [showPass, setShowPass] = useState('password')
+  const handleClickShowPassword = useCallback(() => setShowPass(() => 'text'), [setShowPass])
+  const handleMouseDownPassword = useCallback(() => setShowPass(() => 'password'), [setShowPass])
+  const forPasswordField = useCallback(() => (
+    <InputAdornment position='end'>
+      <IconButton
+        aria-label='toggle password visibility'
+        onClick={handleClickShowPassword}
+        onMouseDown={handleMouseDownPassword}
+      >
+        {showPass === 'text' ? <Visibility /> : <VisibilityOff />}
+      </IconButton>
+    </InputAdornment>
+  ), [handleClickShowPassword, handleMouseDownPassword, showPass])
   return (
     <Controller
       name={name}
@@ -28,8 +47,11 @@ function InputComponent ({
           error={!!errors[name]}
           helperText={errors[name]?.message}
           id={name}
-          type={type}
+          type={type === 'password' ? showPass : type}
           label={label || name}
+          InputProps={{
+            endAdornment: type === 'password' ? forPasswordField() : null
+          }}
           {...initialStyles}
           {...styles}
         />}
