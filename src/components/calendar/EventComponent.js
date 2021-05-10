a825d7e0823bf776a9d25d8f18731db973b9c1ef
Mark Chigrin <ilmpc@ya.ru>
2021-05-10 17:34:18 +0000

    Added timedate picker

---
+++ src/components/calendar/EventComponent.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react'
+import { DatePicker, MuiPickersUtilsProvider } from '@material-ui/pickers'
+import dateUtils from '@date-io/dayjs'
+import { TextField } from '@material-ui/core'
+
+function EventComponent (props) {
+  const [selectedDate, handleDateChange] = useState(new Date())
+  return (
+    <>
+    <form>
+    <MuiPickersUtilsProvider utils={dateUtils}>
+      <DatePicker
+        disableToolbar
+        variant='inline'
+        label='Only calendar'
+        helperText='No year selection'
+        value={selectedDate}
+        onChange={handleDateChange}
+      />
+    </MuiPickersUtilsProvider>
+    </form>
+    </>
+  )
+}
+
+export default EventComponent
