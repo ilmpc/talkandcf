3ff19a64c07937cf7aa6e994d5593096953526de
Khadgow <ostapponevg@gmail.com>
2021-05-12 11:42:27 +0700

    fix conflict

--- src/store/root-saga.js
+++ src/store/root-saga.js
@@ -1,19 +1,13 @@
 import { all, call } from 'redux-saga/effects'
 import user from '../ducks/users'
-<<<<<<< HEAD
 import utils from '../ducks/utils'
-=======
 import events from '../ducks/events'
->>>>>>> 8817c7f (add logic in notifications)
 
 const rootSaga = function * () {
   yield all([
     call(user.sagas),
-<<<<<<< HEAD
-    call(utils.sagas)
-=======
+    call(utils.sagas),
     call(events.sagas)
->>>>>>> 8817c7f (add logic in notifications)
   ])
 }
 export default rootSaga
